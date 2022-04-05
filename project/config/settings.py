1953187b1e3e7347c6ad35d71469edcb38b06434
Joseph Victor Zammit <jvzammit@gmail.com>
2022-04-05 10:05:18 +0200

    Replaces sqlite with local postgres docker container. Adds dj-database-url and psycopg2-binary packages.

--- project/config/settings.py
+++ project/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,10 +80,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://postgres:tiger@localhost:54320/cph22db"
+    )
 }
 
 
