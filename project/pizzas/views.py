da797a79a20cf0215f7b681916b657815642b34d
Joseph Victor Zammit <jvzammit@gmail.com>
2022-04-05 10:52:11 +0200

    Orders menu items by price ASC.

--- project/pizzas/views.py
+++ project/pizzas/views.py
@@ -3,5 +3,5 @@ from pizzas.models import Pizza
 
 
 def home(request):
-    pizzas = Pizza.objects.all().prefetch_related("toppings")
+    pizzas = Pizza.objects.all().prefetch_related("toppings").order_by("price")
     return render(request, "pizzas/home.html", {"pizzas": pizzas})
